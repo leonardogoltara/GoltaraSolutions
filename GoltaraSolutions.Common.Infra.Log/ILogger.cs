cab6c1db1763316503af4bfa1197df41c1d3be3a
Leonardo Goltara <lsgolt94@LIVE.COM>
2021-02-22 14:19:14 -0300

    add log

---
+++ GoltaraSolutions.Common.Infra.Log/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace GoltaraSolutions.Common.Infra.Log
+{
+    public interface ILogger
+    {
+        void Inicialize(string loggerName);
+        void Debug(string message, [CallerMemberName]string memberName = "");
+        void Info(string message, [CallerMemberName]string memberName = "");
+        void Warn(string message, [CallerMemberName]string memberName = "");
+        void Warn(Exception ex, [CallerMemberName]string memberName = "");
+        void Error(string message, [CallerMemberName]string memberName = "");
+        void Error(Exception ex, [CallerMemberName]string memberName = "");
+        void Fatal(string message, [CallerMemberName]string memberName = "");
+        void Fatal(Exception ex, [CallerMemberName]string memberName = "");
+    }
+}
---
+++ GoltaraSolutions.Common.Infra.Log/Logger.cs
@@ -0,0 +1,12 @@
+﻿namespace GoltaraSolutions.Common.Infra.Log
+{
+    public static class Logger
+    {
+        private static ILogger _log;
+        public static ILogger Log
+        {
+            get { return _log; }
+            set { _log = value; }
+        }
+    }
+}
